e971102f570334aadc6a757e9c1b3ba4d03e269a
Mridul Malviya <mridul.malviya@gmail.com>
2019-09-22 20:28:41 +0530

    Add project files.

---
+++ XamarinFormFunctionality/XamarinFormFunctionality/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinFormFunctionality.Models;
+
+namespace XamarinFormFunctionality.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
